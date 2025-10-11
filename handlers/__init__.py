32cf998aea6fc60b2d4b469c20f84f4a6d2af7d5
Scrodinger <schrodinger.grigorii@gmail.com>
2025-10-11 19:46:21 +0300

    add new comand

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,9 +1,11 @@
 from .info import setup_info
 from .new_user import setup_new_user
 from .products import setup as setup_products
+from .text_edit import setup_handle_text_edit
 
 
 def setup(dispatcher):
     setup_products(dispatcher)
     setup_info(dispatcher)
     setup_new_user(dispatcher)
+    setup_handle_text_edit(dispatcher)
